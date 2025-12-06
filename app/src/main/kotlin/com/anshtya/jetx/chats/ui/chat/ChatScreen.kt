e77b80c5e7cfca5bcb4b9e89bfb6d586ca927a18
Ansh Tyagi <tyagiansh486@gmail.com>
2025-12-06 11:58:43 +0530

    Add imePadding() and navigationBarsPadding() to ChatTextInput

--- app/src/main/kotlin/com/anshtya/jetx/chats/ui/chat/ChatScreen.kt
+++ app/src/main/kotlin/com/anshtya/jetx/chats/ui/chat/ChatScreen.kt
@@ -15,11 +15,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -217,12 +216,10 @@ private fun ChatScreen(
                     onMessageSent(chatInputText)
                     chatInputText = ""
                 },
-                modifier = Modifier.padding(
-                    start = 8.dp,
-                    end = 8.dp,
-                    bottom = WindowInsets.navigationBars.asPaddingValues()
-                        .calculateBottomPadding() + 4.dp
-                )
+                modifier = Modifier
+                    .padding(8.dp)
+                    .imePadding()
+                    .navigationBarsPadding()
             )
         }
     ) {
