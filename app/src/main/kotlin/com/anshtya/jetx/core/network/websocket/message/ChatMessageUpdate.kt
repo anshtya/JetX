7b7d4c86f43451edea79467f28315718ebcc9f85
Ansh Tyagi <tyagiansh486@gmail.com>
2025-12-11 14:21:19 +0530

    Mark ChatMessageUpdate with serializable

--- app/src/main/kotlin/com/anshtya/jetx/core/network/websocket/message/ChatMessageUpdate.kt
+++ app/src/main/kotlin/com/anshtya/jetx/core/network/websocket/message/ChatMessageUpdate.kt
@@ -4,6 +4,7 @@ import com.anshtya.jetx.util.UUIDSerializer
 import kotlinx.serialization.Serializable
 import java.util.UUID
 
+@Serializable
 data class ChatMessageUpdate(
     @Serializable(UUIDSerializer::class)
     val id: UUID,
